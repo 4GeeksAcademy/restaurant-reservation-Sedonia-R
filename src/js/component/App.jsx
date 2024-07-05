3b3d8d1e87cdb649ba1c1d21d031832f962d0209
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-07-05 23:51:46 +0000

    first commit, started project

---
+++ src/js/component/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const App = () => {
+	return (
+		<div>
+			<input type="text" />
+			<input type="text" />
+			<input type="text" />
+			<div class="dropdown"></div>
+		</div>
+	);
+};
+
+export default App;
