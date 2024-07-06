22d6c64b2a3c6b61b93fd6fc99ee3cd58c21fe1f
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-07-06 00:52:47 +0000

    structured basic layout

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 
-
 const App = () => {
 	return (
 		<div>
-			<input type="text" />
-			<input type="text" />
-			<input type="text" />
-			<div class="dropdown"></div>
+			<div className="inputFields container">
+				<input type="text" placeholder="Full Name"/>
+				<input type="text" placeholder="Email Address" />
+				<input type="text" placeholder="Seats" />
+				<div className="dropdown">
+					<button className="btn btn-white dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Select One
+					</button>
+					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+						<a className="dropdown-item" href="#">Regular</a>
+						<a className="dropdown-item" href="#">Vip</a>
+					</div>
+				</div>
+			</div>
+			<div className="reserveButton">
+				<button className="btn btn-primary">add reservation</button>
+			</div>
+			<br/>
+			<div className="row container-fluid data">
+				<div className="col-4">Full Name</div>
+				<div className="col-4">Email</div>
+				<div className="col-2">Seats</div>
+				<div className="col-2">Service</div>
+			</div>
 		</div>
 	);
 };
